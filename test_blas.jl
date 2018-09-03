6fea0a798e87073c2ec627d9ce13d2ffd469b714
lik24 <sonyhammer@gmail.com>
2018-09-03 21:46:22 +0500

    blas

---
+++ test_blas.jl
@@ -0,0 +1,24 @@
+n=1000;
+A = sprand(n,n,.05);
+A = A+A';
+A[1:n+1:n*n] = -sum(A,1);
+A[1]=A[1]-1;
+A[end]=A[end]-1;
+
+B = rand(n);
+B[1]=100;
+B[end]=10;
+@time x = A\B;
+
+F = lufact(A);
+L=F[:L]
+U=F[:U]
+p = F[:p]
+y = BLAS.trsv('l', 'n', 'u', L, B)
+
+Bp=copy(B)
+Bp[p]=B;
+y=L\B[p];
+x1 =U\y;
+x2=copy(x1);
+x2[p]=x1;
