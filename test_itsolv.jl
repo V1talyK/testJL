d5088c70a0bd6324e07a8fe1776c89792ee63173
lik <sonyhammer@gmail.com>
2018-12-05 17:04:41 +0500

    add it_solv

---
+++ test_itsolv.jl
@@ -0,0 +1,17 @@
+using IterativeSolvers
+
+@time x0 = A\b;
+@time x1 = IterativeSolvers.gmres(A, b);
+@time x11 = IterativeSolvers.gmres!(x1, A, b);
+
+@time x2 = IterativeSolvers.cg(A, b);
+x21=zeros(Float64,length(x2));
+x21=x0+1#rand(Float64,length(x2));
+@time IterativeSolvers.cg!(x21, A, b);
+
+
+
+@time x3 = minres(A, b)
+
+sum(abs.(x0-x1))
+sum(abs.(x0-x2))
