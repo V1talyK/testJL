075f6f9c73237409c83dd5a3acccaa5c90e03ba8
lik24 <sonyhammer@gmail.com>
2019-02-02 16:36:34 +0500

    it mrg

--- test_itsolv.jl
+++ test_itsolv.jl
@@ -1,8 +1,8 @@
-using IterativeSolvers
+using IterativeSolvers, Preconditioners
 
 @time x0 = A\b;
 @time x1 = IterativeSolvers.gmres(A, b);
-@time x11 = IterativeSolvers.gmres!(x1, A, b);
+@time x11 = IterativeSolvers.gmres!(x0, A, b);
 
 @time x2 = IterativeSolvers.cg(A, b);
 x21=zeros(Float64,length(x2));
@@ -10,8 +10,9 @@ x21=x0+1#rand(Float64,length(x2));
 @time IterativeSolvers.cg!(x21, A, b);
 
 
+p = CholeskyPreconditioner(A, 2)
 
 @time x3 = minres(A, b)
 
-sum(abs.(x0-x1))
+sum(abs.(x0-x11))
 sum(abs.(x0-x2))
