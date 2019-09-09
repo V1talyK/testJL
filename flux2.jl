31e5470c527d50a955e8230b4d3e2ee10ca79525
lik <sonyhammer@gmail.com>
2019-09-09 17:27:36 +0500

    est

--- flux2.jl
+++ flux2.jl
@@ -1,37 +1,39 @@
 using Flux
 using Flux.Tracker
 
-W = rand(2, 5)
-b = rand(2)
-
-predict(x) = W*x .+ b
-
-loss(x, y) = Flux.mse(m(x), y)
-
-x = rand(50); y = (x.-0.5).^2 # Dummy data
-loss(x, y) # ~
-
-m = Chain(Dense(2, 2, σ),Dense(2, 2, σ))
-
-ps = Flux.params(m)
-opt = Descent(0.1)
-
-data = [(x[j:j+1],y[j:j+1]) for j=1:49]
-data = Iterators.repeated((x, y), 3)
-for i=1:100
-    Flux.train!(loss, ps, data, opt)#cb = ()->println(loss(x,y))
+abc = param(abc)
+function prbl(x,abc)
+    a, b, c = abc;
+    y = a.*x.^2 .+b*x .+ c
 end
 
-m(x[1:2])
+function loss()
+    y = prbl(x,p)
+    sum(abs2.(y0-y)) # loss function
+end
 
-loss(x,y)
+x0 = rand(50); y0 = (x.-0.5).^2 # Dummy data
 
-x2 = rand(50)
-data = [x2[j:j+1] for j=1:49]
-y1 = Tracker.data(m(x2))
+abc = [1,2,3]
+loss()
 
-plt = scatterplot(x2, y1, title = "ET", name = "a=0.1", xlabel = "x", ylabel = "y");
-scatterplot!(plt,x, y, name = "m(x)");
+
+
+m = Chain(Dense(1, 2, σ),Dense(2, 1, σ))
+
+p = param([0,0,0])
+params = Flux.Params([p])
+
+opt = ADAM(0.1);#opt = Descent(0.1)
+
+data = Iterators.repeated((), 100)
+for i=1:5
+ Flux.train!(loss, params, data, opt)#cb = ()->println(loss(x,y))
+end
+
+
+plt = scatterplot(x, y, title = "ET", name = "a=0.1", xlabel = "x", ylabel = "y");
+scatterplot!(plt,x2, y1, name = "m(x)");
 
 #plt = scatterplot(y1, y, title = "ET", name = "a=0.1", xlabel = "x", ylabel = "y");
 display(plt)
