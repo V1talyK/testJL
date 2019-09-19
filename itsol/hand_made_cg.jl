8ac46150d5285ebbcb3f31fef2e49e191b0648cb
lik_home <sonyhammer@gmail.com>
2019-09-20 01:52:32 +0500

    bnm

--- itsol/hand_made_cg.jl
+++ itsol/hand_made_cg.jl
@@ -20,6 +20,7 @@ end
 
 @time x00 = A\b;
 mean(abs.(x0-x00))
+
 A = -A
 CL = cholesky(A)
 x = zeros(size(A,1))
@@ -55,3 +56,47 @@ rho = dot(r,z)
 
     if mod(i,20)==0 println(sum(abs.(b-A*x))) end
 end
+
+cuA = CuArrays.CUSPARSE.CuSparseMatrixCSR(-A);
+CL = cholesky(A)
+CLL = SparseArrays.sparse(CL.L)
+cuCLL =  CuArrays.CUSPARSE.CuSparseMatrixCSR(CLL);
+x = CuArrays.zeros(eltype(cuA),size(A,1))
+r = cuB-cuA*x;
+z = copy(r)
+p = copy(z)
+rho = [dot(r,z)]
+q = copy(r)
+@time for i=1:200
+    t = CUSPARSE.sv_solve('T','L',one(Float64),cuCLL,r,infoT,'O')
+    z[:] = CUSPARSE.sv_solve('N','L',one(Float64),cuCLL,t,info,'O')
+    #t = cuCLL\r;
+    #z = cuCLL'\t
+    #global rho
+    #rho = dot(r,r)
+    rhop = copy(rho)
+    rho[1] = dot(r,z)
+    if i==1
+        p[:] = copy(z)
+    else
+        β = rho[1]/rhop[1]
+        z[:] = z+β*p
+        p[:] = z
+    end
+
+
+    #y = α ∗ op ( A ) ∗ x + β ∗ y
+    q[:] = cuA*p
+    temp = dot(p,q)
+    α = rho[1]/temp;
+
+    #ak = dr0/dot((A*z0),z0)
+    x[:] += α*p
+    r[:] -= α*q
+    nrmr = norm(r)
+
+    if mod(i,20)==0 println(sum(abs.(cuB-cuA*x))) end
+end
+
+infoT = sv_analysis('T', 'S', 'L', cuCLL,'O')
+info = sv_analysis('N', 'S', 'L', cuCLL,'O')
