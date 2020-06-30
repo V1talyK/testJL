bb5c943f270fd39ca6f3f6a789d7592b8a0085f7
lik_home <sonyhammer@gmail.com>
2020-07-01 00:27:16 +0500

    пилим

---
+++ make_opigin.jl
@@ -0,0 +1,34 @@
+using SparseArrays
+nt = 20
+upFlag = falses(nt)
+upFlag[[1,7,11,14,16,17]].=true;
+nl = 3;
+
+At = zeros(Int64,nt);
+ki = findall(upFlag)
+for (k,v) in enumerate(ki)
+    At[v:nt] .= k
+end
+
+fe = spzeros(Int64,nt,nt);
+up = zeros(Int64,nt,nt);
+cf = spzeros(Int64,nt,nt);
+cmb = CartesianIndex.(zeros(Int64,nl),zeros(Int64,nl));
+rc = Vector(undef,0)
+for r=1:nt
+    for c=1:nt
+        if r<=c<r+nl
+            push!(rc,[r,c])
+            fe[r,c] = c-r
+            up[r,c] = At[c]
+            cf[r,c] = CartesianIndex(fe[r,c],up[r,c]) != cmb[c-r+1]
+            cmb[c-r+1] = CartesianIndex(fe[r,c],up[r,c])
+        end
+    end
+end
+
+fev = [fe[irc[1],irc[2]] for irc in rc]
+upv = [up[irc[1],irc[2]] for irc in rc]
+
+ci = CartesianIndex.(fev,upv)
+unique(ci)
