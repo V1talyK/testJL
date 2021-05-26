33b5b41645fa9f308f1b3e2b27ed75f1439f722d
lik <lk.24@yandex.ru>
2021-05-27 00:59:17 +0500

    htifkrf

--- solv/make_solv.jl
+++ solv/make_solv.jl
@@ -3,15 +3,33 @@ ACL = cholesky(mA);
 
 @time x0 = ACL\b
 
-L = ACL.L;
-LL = LowerTriangular(sparse(L))
+@btime L = ACL.L;
+@btime LL = LowerTriangular(sparse(L))
+Lt = copy(LL');
 
 p = ACL.p;
 bp = b[p];
+LL[:,p]
 @time y .= LL\bp;
-@time x .= L'\y;
+@time x .= Lt\y;
 
 sum(abs,x.-x0[p])
 
 
-ldiv!(LL,bp)
+@btime ldiv!(y, LL,bp)
+@btime ldiv!(x, Lt,y)
+@btime x0 = ACL\b
+
+@btime begin
+    ldiv!(y, LL,bp)
+    ldiv!(x, Lt,y)
+end
+
+LinearAlgebra.BLAS.trsv('L', 'N', 'N', LL, bp)
+
+LinearAlgebra.BLAS.set_num_threads(2)
+
+@profiler ldiv!(y, LL,bp)
+
+
+permute(sparse(L), collect(1:16610), p)
