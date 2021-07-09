15f810775851015c3ddfdce8eefbab020bdb5fd6
lik <lik.24@yandex.ru>
2021-07-09 17:32:06 +0500

    23

--- cg/fb.jl
+++ cg/fb.jl
@@ -1,48 +1,8 @@
-function backward_substitution!(U, y::AbstractVector)
-    n = size(U,1)
-    @inbounds for col = n : -1 : 1
-
-        # Substitutions
-        for idx = U.data.colptr[col + 1] - 1 : -1 : U.data.colptr[col] + 1
-            y[col] -= U.data.nzval[idx] * y[U.data.rowval[idx]]
-        end
-
-        # Final answer for y[col]
-        y[col] /= U.data.nzval[U.data.colptr[col]]
-    end
-
-    y
-end
-
-function forward_substitution!(y, L, b::AbstractVector)
-    y = copy(b)
-    @inbounds for col = 1 : L.data.n - 1
-        for idx = L.data.colptr[col] : L.data.colptr[col + 1] - 1
-            y[L.data.rowval[idx]] -= L.data.nzval[idx] * y[col]
-        end
-    end
-
-    y
-end
-
-
-function forward_substitution1!(L, y::AbstractVector)
-    @inbounds for col = 1 : L.data.n - 1
-        # for idx = L.data.colptr[col] : L.data.colptr[col + 1] - 1
-        #     y[L.data.rowval[idx]] -= L.data.nzval[idx] * y[col]
-        # end
-        idx = L.data.colptr[col] : L.data.colptr[col + 1] - 1
-        iy = view(L.data.rowval,idx)
-        z = view(L.data.nzval,idx)
-        #y[iy] .= view(y,iy) .- z.*y[col]
-        nmn!(view(y,iy),z,-y[col])
-    end
-
-    y
-end
-
-function nmn!(a,b,c)
-    axpy!(c, b, a)
+function make_CL_in_julia(ACL, nth = 1)
+    LL = sparse(ACL.L)
+    UU = copy(LL')
+    x_temp = zeros(LL.n, nth)
+    return (L = LL, U = UU, p = ACL.p, x_temp)
 end
 
 @btime $x .= $ACL\$b
@@ -66,58 +26,31 @@ sum(abs,x[CL.p].-x1)
 @btime ldiv!($x1,$UU,$y)
 
 
-function frw_sb0!(x, S, b)
-    x .= 0;
-    @inbounds for col = 1:S.n
-        idx = S.colptr[col] : S.colptr[col + 1] - 1
-        #println(idx," ",S.rowval[idx])
-        @inbounds for (k,v) in enumerate(idx)
-            #println(j)
-            j = S.rowval[v]
-            if j == col
-                x[j] = (b[j] + x[j])/S.nzval[idx[k]]
-            else
-                x[j] -=  S.nzval[idx[k]] * x[col]
-            end
-        end
-    end
-    return x
-end
-
-
-function frw_sbt!(x, S, b)
-    x .= 0;
-    @inbounds for col = 1:S.n
-        idx = S.colptr[col]+1 : S.colptr[col + 1] - 1
-        #println(idx," ",S.rowval[idx])
-        x[col] = (b[col] + x[col])/S.nzval[S.colptr[col]]
-        v1 = view(S.rowval,idx)
-        #for i in eachindex(idx,v1)
-             #j = S.rowval[v]
-        @tturbo x[v1] .= view(x,v1) .- view(S.nzval,idx) .* x[col]
-        #fg.(view(x,v1),view(S.nzval,idx),x[col])
-        #end
-        # idx2 = view(idx,2:length(idx))
-        # row = view(S.rowval,idx2)
-        # x[row] = view(x,row) .- S.nzval[idx2] .* x[col]
-    end
-    return x
-end
-
-function frw_sb!(x, S, b)
+function forward_substit!(x, S, b)
     x .= 0;
     @fastmath @inbounds for col = 1:S.n
         idx = S.colptr[col]+1 : S.colptr[col + 1] - 1
         #println(idx," ",S.rowval[idx])
         x[col] = (b[col] + x[col])/S.nzval[S.colptr[col]]
         #v1 = view(S.rowval,idx)
-        @simd for v in idx
+        for v in idx
              x[S.rowval[v]] -=  S.nzval[v] * x[col]
         end
     end
-    return x
 end
 
+function backward_substit!(x, UU, b)
+    x .= 0;
+    @fastmath @inbounds for col = UU.n:-1:1
+        idx = UU.colptr[col + 1]-2 :-1 : UU.colptr[col]
+        #println(idx," ",S.rowval[idx])
+        x[col] = (b[col] + x[col])/UU.nzval[UU.colptr[col+1]-1]
+
+        for v in idx
+             x[UU.rowval[v]] -=  UU.nzval[v] * x[col]
+        end
+    end
+end
 
 
 x[1] = b[1]/S.nzval[1]
@@ -143,3 +76,12 @@ x[5] = x[5] - UU.nzval[8] * x[6]
 x[5] = (b[5] + x[5]) /UU.nzval[8]
 x[2] = x[2] - UU.nzval[7] * x[5]
 x[1] = x[1] - UU.nzval[6] * x[5]
+
+x[4] = (b[4] + x[4])/UU.nzval[5]
+
+x[3] = (b[3] + x[3])/UU.nzval[4]
+x[1] = x[1] - UU.nzval[3]*x[3]
+
+x[2] = (b[2] + x[2])/UU.nzval[2]
+
+x[1] = (b[1] + x[1])/UU.nzval[1]
---
+++ flux/data_set.jl
@@ -0,0 +1,50 @@
+cok=Dict("cma"=>"9c32c492-8265-46f0-a50d-0969d471d303","cdb"=>"","csql"=>"30","ch"=>"ch30"); #Dev 66
+pl = "SELECT
+iter,
+wi,
+sm,
+sy,
+em,
+ey,
+array(countEqual(st, 0), countEqual(st, 1),countEqual(st, 2), countEqual(st, 3)) as st,
+gw,
+ws,
+kp,
+pw,
+qw,
+ppl,
+Jx,
+lam,
+aqm,
+pvt,
+nc,
+nw,
+nt
+FROM default._ccord_data_60e400887ddd740040cf4645_ch_scheme_60e40b687ddd740040cf4646 FORMAT JSONCompact"
+
+@time data = chPost(pl,cok)["data"];
+
+
+x_train = getindex.(data,9)
+y_train = getindex.(data,14)
+
+
+model = Chain(Dense(1377, 500),Dense(500, 8, σ))
+model(x_train[1])
+loss(x, y) = Flux.Losses.mse(model(x), y)
+loss(x_train[1], y_train[1])
+
+data = Vector(undef,450)
+for (k,v) in enumerate(zip(x_train, y_train))
+    data[k] = Float32.(v[1]), Float32.(v[2])
+end
+
+parameters = Flux.params(model)
+for i = 1:20
+  train!(loss, parameters, data[1:400], opt)
+  #println(loss(data[i][1],data[i][2]))
+end
+
+
+mean(loss.(getindex.(data,1),getindex.(data,2)))
+loss(data[1][1],data[1][2])
