3cc6d0c92e7ff1f50509d4fe00fbce10af1d8719
lik <lik.24@yandex.ru>
2021-07-14 14:34:17 +0500

    допил до другого формата

--- make_sint/make_surf.jl
+++ make_sint/make_surf.jl
@@ -55,3 +55,33 @@ function writeToFile(name,D)
     writeToPipe(ioW, D...)
     close(ioW)
 end
+
+function writeToPipe(io, args...)
+    k = 0
+
+    newline = '\n'
+    delim = '\t'
+    @inbounds while k < length(args[1])
+        k += 1
+        ncols = length(args)
+        @inbounds for i = 1:ncols
+            v = getStr(args[i][k])
+            write(io, "$v")
+            write(io, ifelse(i == ncols, newline, delim))
+        end
+
+    end
+    close(io)
+    return true
+end
+
+
+@inline getStr(v::Number) = ifelse(isnan(v), "\\N", string(v))
+#@inline getVal(v::Array{T,1}) where T <: Number = (length(v) > 0) ? "["*join(map(x -> string(x), v),", ")*"]" : "\\N"
+#@inline getVal(v::Array{T,1}) where T <: AbstractString = (length(v) > 0) ? "["*join(map(x -> "'$x'", v),", ")*"]" : "\\N"
+@inline getStr(v::Tuple) = string(v)
+@inline getStr(v::Date) = "$v"
+@inline getStr(v::String) = "$v"
+@inline getStr(v::Array{Float64,1}) = "$v"
+@inline getStr(v::Array{<:Signed,1}) = "$v"
+#@inline getVal(v::Nothing) = "\\N"
--- make_sint/reg_gen.jl
+++ make_sint/reg_gen.jl
@@ -1,3 +1,4 @@
+using Dates
 well_prod_name = collect("PROD$i" for i = 1:32)
 well_inj_name = collect("INJE$i" for i = 1:4)
 well_name = vcat(well_prod_name, well_inj_name)
@@ -13,7 +14,8 @@ for (k,v) in enumerate(Iterators.product(well_name, vd))
     push!(D,(v[1], "$(tmp[9:10]).$(tmp[6:7]).$(tmp[1:4])", "BHPT", BHPT[k]))
 end
 
-writeToFile("operating_mode_5",[getindex.(D,1),getindex.(D,2),getindex.(D,3),getindex.(D,4)])
+
+writeToFile("operating_mode_11",[getindex.(D,1),getindex.(D,2),getindex.(D,3),getindex.(D,4)])
 
 
 #operating_mode_1 - база
@@ -21,3 +23,26 @@ writeToFile("operating_mode_5",[getindex.(D,1),getindex.(D,2),getindex.(D,3),get
 #operating_mode_3 - снижение закачки (понижение забойки на наг.)
 #operating_mode_4 - отключение половины доб. скв.
 #operating_mode_5 - отключение наг. скв.
+D = Vector(undef,0)
+for (kt,vt) in enumerate(vd)
+    tmp = "$(vt)"
+    push!(D,"DATES")
+    push!(D,"$(tmp[9:10]) $(uppercase(monthabbr(vt))) $(tmp[1:4]) /")
+    push!(D,"/")
+    push!(D,"")
+    push!(D,"WCONPROD")
+    for (k,v) in enumerate(well_prod_name)
+       push!(D, "$v  1*  BHP  1*  1*  1*  1*  1*  $(BHPT[k,kt])  /")
+    end
+    push!(D,"/")
+    push!(D,"")
+    push!(D,"WCONINJE")
+    for (k,v) in enumerate(well_inj_name)
+        push!(D,"$v WATER  OPEN  RATE  10000  1* $(BHPT[k+32,kt])  /")
+    end
+    push!(D,"/")
+    push!(D,"/")
+    push!(D,"")
+end
+
+writeToFile("export/operating_mode_1_v2",[D])
