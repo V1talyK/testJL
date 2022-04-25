958c199df60804a09722a6b750be8f4b54c6dc90
lik <lik.24@yandex.ru>
2022-04-25 07:55:13 +0500

    23

--- cnl/chnl_t.jl
+++ cnl/chnl_t.jl
@@ -1,54 +1,32 @@
-x = zeros(length(b),8)
-ch = Channel{Any}(4)
-ts = Threads.@spawn begin
-    thr_id = Threads.threadid()
-    for item in ch
-        CL=cholesky(mA);
-        x[:,thr_id] = CL\items;
-        println(extrema(view(x,:,thr_id)))
-    end
+function foo(vl, chnl)
+  for v in vl
+    v=v*2
+    put!(chnl, v)
+  end
+end
+
+vl = rand(100)
+my_channel = Channel( (channel_arg) -> foo(vl, channel_arg) )
+
+for i in my_channel
+    i |> println
 end
 
 
-put!(ch,b)
-for i=1:100
-    put!(ch,rand(length(b)))
+put!(my_channel, 2)
+x = [similar(b) for i in 1:Threads.nthreads()]
+
+mcnl1 = Channel( (bb) -> foo1(x,mA, bb), spawn=true)
+mcnl2 = Channel( (bb) -> foo1(x,mA, bb), spawn=true)
+
+function foo1(x,mA,cnl)
+  for v in cnl
+    println(Threads.threadid())
+    x[Threads.threadid()] .= mA*v
+  end
 end
 
-x = zeros(length(b),8)
-CL8=[cholesky(mA) for i=1:8];
-CL=cholesky(mA)
-
-function foo(x,CL8)
-    @sync for i=1:100
-        Threads.@spawn begin
-            thr_id = Threads.threadid()
-            @views cholesky!(CL8[thr_id],mA)
-            @views x[:,thr_id] = CL8[thr_id]\b;
-            #x[:,thr_id] = CL\b;
-        end
-    end
+@time for i=1:10
+  put!(mcnl1,b)
+  #put!(mcnl2,b)
 end
-
-@time foo(x,CL8)
-@btime foo($x,$CL8)
-
-
-ch1 = Channel{Any}(3)
-k=0
-f2() = f1(ch1)
-ts = @task begin
-    for item in ch1
-        thr_id = Threads.threadid()
-        println(thr_id)
-        println(item)
-        k+=1
-    end
-end
-
-ts = @task f2()
-schedule(ts)
-yield()
-put!(ch1,2)
-istaskstarted(ts)
-istaskdone(ts)
