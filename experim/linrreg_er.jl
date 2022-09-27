57cc6ba4d0e60bee49746b518768e4b7a49b730c
lik <lik.24@yandex.ru>
2022-09-27 17:27:39 +0500

    расчёт погрешности для линейной регрессии

--- experim/linrreg_er.jl
+++ experim/linrreg_er.jl
@@ -1,4 +1,4 @@
-using UnicodePlots, StatsBase
+using UnicodePlots, StatsBase, HypothesisTests
 a = 0.5
 b = 2.5
 foo(x) = a*x+b
@@ -18,5 +18,22 @@ b1 = y1 - a1*x1
 lineplot!(plt,x,a1.*x.+b1)
     println(plt)
 
+N = length(x)
+Sy2 = 1/(N-2)*(sum(y.^2) - a1*sum(x.*y)-b1*sum(y))
+Sa2 = Sy2/(sum(x.^2) - N*x1^2)
+Sb2 = Sy2*sum(x.^2)/(N*sum(x.^2) - N*x1^2)
 
-Sy2 = 1/(length(x)-2)*(sum)
+tp = 2.13
+Δa = tp*sqrt(Sa2)
+Δb = tp*sqrt(Sb2)
+
+"$a1+$Δa"
+"$b1+$Δb"
+
+foo_dy(x) = sqrt(x^2*Δa*Δa + 1 * Δb*Δb)
+Δy = foo_dy.(x)
+plt = histogram(Δy)
+    println(plt)
+
+plt = scatterplot(x,Δy)
+    println(plt)
