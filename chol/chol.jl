7d891d9da1757179ddc1c455d1f7b5fefd92a302
lik <lik.24@yandex.ru>
2023-03-08 23:08:31 +0500

    cl dev

--- chol/chol.jl
+++ chol/chol.jl
@@ -1,26 +1,89 @@
 function hand_cholS(A)
-    L = spzeros(size(A))
+    L = sparse(LowerTriangular(A))
+    L.nzval.=0.0
     n = size(A,1)
     L[1,1] = sqrt(A[1,1])
+    Lr = zeros(Int64,0)
+    Lc = zeros(Int64,0)
+    Lv = zeros(Float64,0)
+    Ld = zeros(n)
+
+    push!(Lr,1)
+    push!(Lc,1)
+    push!(Lv,sqrt(A.nzval[1]))
+    Ld[1] = sqrt(A.nzval[1])
+
     for i = A.colptr[1]+1:A.colptr[2]-1
-        L[A.rowval[i],1] = A[A.rowval[i],1]/L[1,1]
+        #L[A.rowval[i],1] = A[A.rowval[i],1]/L[1,1]
+        L[A.rowval[i],1] = A.nzval[i]/Ld[1]
+        push!(Lr,A.rowval[i])
+        push!(Lv,A.nzval[i]/Lv[1])
     end
+    push!(Lc,length(Lr)+1)
 
     for j = 2:n
         s = 0.0
-        for k=1:j-1
-            s+=L[j,k]^2
+        #s0 = 0.0
+        # for k = 1:j-1
+        #     s+=L[j,k]^2
+        #     #s+=v^2
+        # end
+        #s = 0
+        for k = Lc[1]:Lc[j]-1
+            if Lr[k]==j
+                s+=Lv[k]^2
+            end
         end
-        L[j,j] = sqrt(A[j,j] - s)
+        #s0=sum(Lv[j].^2)
+        #println(j," ",s," ",s0)
+        for i = A.colptr[j]:A.colptr[j+1]-1
+            if A.rowval[i]==j
+                Ld[j] = sqrt(A.nzval[i] - s)
+                push!(Lr,j)
+                push!(Lv,sqrt(A.nzval[i] - s))
+            end
+        end
+        # for i = A.colptr[j]:A.colptr[j+1]-1
+        #     if A.rowval[i]>j
+        #         s0 = 0.0
+        #         for k = 1:j-1
+        #             s0+=L[A.rowval[i],k]*L[j,k]
+        #         end
+        #         L[A.rowval[i],j] = (A.nzval[i]-s0)/L[j,j]
+        #     end
+        # end
         for i = j+1:n#A.colptr[j]:A.colptr[j+1]-1
             s = 0.0
-            for k = 1:j-1
-                s += L[i,k]*L[j,k]
+            # for k = 1:j-1
+            #     s += L[i,k]*L[j,k]
+            # end
+            s = 0.0
+            sj = 0
+            si = 0
+            for k1=1:j-1
+                for k = Lc[k1]:Lc[k1+1]-1
+                    if Lr[k]==j
+                        sj = Lv[k]
+                    end
+                    if Lr[k]==i
+                        si = Lv[k]
+                    end
+                end
+                s+=si*sj
+                si = 0
+                sj = 0
             end
-            L[i,j] = 1/L[j,j]*(A[i,j]-s)
+
+            #L[i,j] = 1/Ld[j]*(A[i,j]-s)
+            push!(Lr,i)
+            push!(Lv,1/Ld[j]*(A[i,j]-s))
+            #push!(Lv[i],1/Ld[j]*(A[i,j]-s))
         end
+        push!(Lc,length(Lr)+1)
     end
-    return L
+
+    L[1:n+1:end].=Ld
+    return L, (Lr,Lc,Lv)
 end
 
 A.colptr
--- chol/test_chol.jl
+++ chol/test_chol.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, BenchmarkTools, Profile
+using LinearAlgebra, BenchmarkTools, Profile, SparseArrays
 n = 100
 A = rand(n,n); A = A.+A'; A = diagm(sum(A,dims=2)[:].+1).-A
 mA = sprand(n,n,0.3); mA = mA.+mA'; mA = diagm(sum(mA,dims=2)[:].+1).-mA
@@ -25,7 +25,18 @@ sum(abs, L*L'.-A)
 @btime hand_cholS($mA);
 @btime hand_chol($mA);
 @time cholesky(mA);
-L = hand_cholS(mA);
+@profiler hand_cholS(mA);
+L, L1 = hand_cholS(mA);
 sum(abs, L*L'.-mA)
 
+c = Vector(undef,length(L1[1]))
+for i=1:length(L1[2])-1
+    ind = convert(Array{Int64,1},L1[2][i]:(L1[2][i+1]-1))
+    c[ind] = i*ones(Int64,length(ind));
+end
+Ls = SparseArrays.sparse(L1[1],c, L1[3])
+sum(abs, Ls*Ls'.-mA)
+
 A = copy(mA)
+
+findall(LowerTriangular(A).!=0)
