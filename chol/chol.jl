2bf03f930177fe40b8e5f63549b4f8ffc21546eb
lik <lik.24@yandex.ru>
2023-03-29 23:24:45 +0500

    ac chol

--- chol/chol.jl
+++ chol/chol.jl
@@ -3,7 +3,7 @@ function hand_cholS(A)
     L.nzval.=0.0
     n = size(A,1)
     L[1,1] = sqrt(A[1,1])
-    Lr = zeros(Int64,0)
+    Lr = zeros(Int64,Int64(n*n/2))
     Lc = zeros(Int64,0)
     Lv = zeros(Float64,0)
 
@@ -18,20 +18,26 @@ function hand_cholS(A)
 
     Ld = zeros(n)
 
-    push!(Lr,1)
+    Lr[1] = 1
     push!(Lc,1)
     push!(Lv,sqrt(A.nzval[1]))
-    Ld[1] = sqrt(A.nzval[1])
 
+    Lr2[1] = 1
+    push!(Lc2,1)
+    push!(Lv2,sqrt(A.nzval[1]))
+
+    Ld[1] = sqrt(A.nzval[1])
+    kr = 1
     for i = A.colptr[1]+1:A.colptr[2]-1
         #L[A.rowval[i],1] = A[A.rowval[i],1]/L[1,1]
         L[A.rowval[i],1] = A.nzval[i]/Ld[1]
-        push!(Lr,A.rowval[i])
+        kr+=1
+        Lr[kr] = A.rowval[i]
         push!(Lv,A.nzval[i]/Lv[1])
 
         LL[A.rowval[i]][1] = A.nzval[i]/Lv[1]
     end
-    push!(Lc,length(Lr)+1)
+    push!(Lc,kr+1)
 
     for j = 2:n
         s = 0.0
@@ -52,7 +58,9 @@ function hand_cholS(A)
         for i = A.colptr[j]:A.colptr[j+1]-1
             if A.rowval[i]==j
                 Ld[j] = sqrt(A.nzval[i] - s)
-                push!(Lr,j)
+                kr+=1
+                Lr[kr] = j
+                #push!(Lr,j)
                 push!(Lv,sqrt(A.nzval[i] - s))
                 LL[A.rowval[i]][j] = sqrt(A.nzval[i] - s)
             end
@@ -72,46 +80,25 @@ function hand_cholS(A)
             end
         end
         for i = j+1:n#A.colptr[j]:A.colptr[j+1]-1
-            s = 0.0
-            # for k = 1:j-1
-            #     s += L[i,k]*L[j,k]
-            # end
-            s0 = 0.0
-            sj = 0
-            si = 0
-            # for k1=1:j-1
-            #     # for k = Lc[k1]:Lc[k1+1]-1
-            #     #     if Lr[k]==j
-            #     #         sj = Lv[k]
-            #     #     end
-            #     #     if Lr[k]==i
-            #     #         si = Lv[k]
-            #     #     end
-            #     # end
-            #     # s+=si*sj
-            #     # si = 0
-            #     # sj = 0
-            #     #println(j,i,k1)
-            #     s+=LL[i][k1]*LL[j][k1]
-            # end
             s = LinearAlgebra.BLAS.dot(j-1,LL[i],1,LL[j],1)
-            # println(j," ",i," ",s0," ",s)
-            # if j==4
-            #     er
-            # end
-
-            #L[i,j] = 1/Ld[j]*(A[i,j]-s)
-            push!(Lr,i)
+            s = 0.0
+            @inbounds for k=1:j-1
+                s+=LL[i][k]*LL[j][k]
+            end
+            kr+=1
+            Lr[kr] = i
+            #push!(Lr,i)
 
             s1 = LL[i][j]
             LL[i][j] = 1/Ld[j]*(s1-s)
             push!(Lv,LL[i][j])
             #push!(Lv[i],1/Ld[j]*(A[i,j]-s))
         end
-        push!(Lc,length(Lr)+1)
+        push!(Lc,kr+1)
     end
 
     L[1:n+1:end].=Ld
+    Lr = Lr[1:kr]
     return L, (Lr,Lc,Lv)
 end
 
