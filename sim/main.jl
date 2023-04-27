ffa8008c27c8ae664d4cf940f24abdce9f5d73d1
lik <lik.24@yandex.ru>
2023-04-27 17:47:46 +0500

    разобраться с матрицей ковариации точности модели

--- sim/main.jl
+++ sim/main.jl
@@ -1,17 +1,17 @@
-using  UnicodePlots, StatsBase, Term
+using  UnicodePlots, StatsBase, Term, LinearAlgebra
 include(joinpath(Base.source_path(),"../../sim/sim9.jl"))
 
 
 nw, nt  = 9, 100;
 qw = rand(nw,nt)
-qw[1,:] .= 1;  qw[1,50:60] .= 1.5;
+qw[1,:] .= 1;  qw[1,50:60] .= 2;
 qw[2,:] .= 1;
 qw[3,:] .= 1;
 qw[4,:] .= 1;
 qw[5,:] .= -8; qw[5,25:50] .= -6;
 qw[6,:] .= 1;
 qw[7,:] .= 1;
-qw[8,:] .= 1;
+qw[8,:] .= 1;   qw[8,75:80] .= 5;
 qw[9,:] .= 1;
 
 Tt = rand(nw)
@@ -22,15 +22,59 @@ bet = 1e-4;
 
 AA, bb, eVp = make9p(Tt, Pa, nw, bet)
 
-PM = sim(qw, nt, AA, bb, P0, eVp)
-plt = Vector(undef, Int64(ceil(nw/3)))
-for (k,v) in enumerate(Iterators.partition(1:nw,3))
-    plt[k] = lineplot(PM[v[1],:], ylim = [floor(minimum(PM)),ceil(maximum(PM))])
-    for i in v[2:end]
-        lineplot!(plt[k], PM[i,:])
-    end
-end
+PM, dP_dp0, dP_dVp = sim(qw, nt, AA, bb, P0, eVp)
+plot_P(PM, nw)
 
-grid(panel.(plt[1:3]); layout=(1, nothing)) |> print
+dbet = eVp.*0.5
+
 #grid(panel.(plt[4:6]); layout=(1, nothing)) |> print
 #grid(panel.(plt[7:9]); layout=(1, nothing)) |> print
+Pk0 = zeros(nw, nw);
+Pk0[1:nw+1:nw*nw].=(2/3)^2
+Qk = [zeros(nw, nw) for _ in 1:nt]
+for t=1:nt
+    Qk[t] .= dP_dVp[t].*dbet
+    Qk[t] .= cov(dP_dVp[1][:,:],dP_dVp[1][:,:]')
+end
+
+Hk = diagm(0 => ones(nw))
+zk = PM .+ rand(-0.5:0.1:0.5,nw, nt)
+sid = mean(abs2,z.-P_0)
+Rk = diagm(0 => sid*10*ones(nw))#(1/3)^2
+I1 = diagm(0 => ones(nw))#(1/3)^2
+
+p0 = Paq*ones(nw)
+p_sim = similar(qw)
+p_kor = similar(qw)
+simt_f = make_simt_f(AA,bb,eVp)
+dPx = dP_dp0
+
+
+
+for t=1:nt
+    p_sim[:,t] = simt_f(p0,qw[:,t])
+    Pkt  = dPx[t]*Pk0*dPx[t]' .+ Qk[t]
+    yk = zk[:,t] .- p_sim[:,t]
+    Sk = Hk*Pkt*Hk' + Rk
+    Kk = Pkt*Hk'/Sk
+    p_kor[:,t] = p_sim[:,t] + Kk*yk
+    Pkk = (I1 - Kk*Hk)*Pkt
+    Pk0 = Pkk
+    # PP0 = PP[t]
+    #p0 .= p_sim[:,t]
+    p0 .= p_kor[:,t]
+end
+
+plot_P(p_sim, nw)
+
+plt = lineplot(PM[1,:], ylim = [floor(minimum(PM[1,:])), ceil(maximum(PM[1,:]))])
+    lineplot!(plt, p_sim[1,:])
+    println(plt)
+
+plt = lineplot(PM[1,:], ylim = [floor(minimum(PM[1,:])), ceil(maximum(PM[1,:]))])
+    lineplot!(plt, zk[1,:])
+    println(plt)
+
+plt = lineplot(p_sim[1,:], ylim = [floor(minimum(PM[1,:])), ceil(maximum(PM[1,:]))])
+    lineplot!(plt, zk[1,:])
+    println(plt)    
--- sim/sim9.jl
+++ sim/sim9.jl
@@ -28,19 +28,66 @@ end
 
 function sim(qw, nt, AA, bb, P0, eVp)
     PM = zeros(size(qw))
+    dP_dp0 = Vector(undef, nt)
+    dP_dVp = Vector(undef, nt)
     p0 = copy(P0)
     for t = 1:nt
         simt!(view(PM,:,t),AA,bb,view(qw,:,t), p0, eVp)
+        dP_dp0[t] = zeros(length(eVp),length(eVp))
+        dP_dVp[t] = zeros(length(eVp),length(eVp))
+        cacl_dP_dp0!(dP_dp0[t],AA,eVp)
+        cacl_dP_dVp!(dP_dVp,AA,view(PM,:,t),p0)
         p0 .= view(PM,:,t)
     end
-    return PM
+    return PM, dP_dp0, dP_dVp
 end
 
 function simt!(Pt,AA,bt,qt, p0, eVp)
     Pt .= AA\(-bt.+qt .- p0.*eVp)
 end
 
+function cacl_dP_dp0!(dP_dp0,AA,eVp)
+    bb = zeros(length(eVp))
+    for (k,v) in enumerate(eVp)
+        bb[k] = - eVp[k]
+        dP_dp0[:,k] .= AA\bb
+        bb[k] = 0.0;
+    end
+end
+
+function cacl_dP_dVp!(dP_dVp,AA,pt,p0)
+    bb = zeros(length(pt))
+    dA_dVp = zeros(length(pt))
+    for (k,v) in enumerate(pt)
+        bb[k] = - p0[k]
+        dA_dVp[k] = 1.0;
+        dP_dVp[:,k] .= AA\(bb .- dA_dVp.*pt)
+        bb[k] = 0.0;
+        dA_dVp[k] = 0.0;
+    end
+end
+
+function make_simt_f(AA,bt,eVp)
+    x1 = zeros(length(bt))
+    function simt_f(x,u)
+        simt!(x1,AA,bt,u, x, eVp)
+        return x1
+    end
+    return simt_f
+end
+
 panel(p; kw...) = begin
   p.margin[] = p.padding[] = 0  # make plots more compact
   Panel(string(p; color=true); style="hidden", fit=true, kw...)
+end
+
+function plot_P(PM, nw)
+    plt = Vector(undef, Int64(ceil(nw/3)))
+    for (k,v) in enumerate(Iterators.partition(1:nw,3))
+        plt[k] = lineplot(PM[v[1],:], ylim = [floor(minimum(PM)),ceil(maximum(PM))])
+        for i in v[2:end]
+            lineplot!(plt[k], PM[i,:])
+        end
+    end
+    grid(panel.(plt[1:3]); layout=(1, nothing)) |> print
 end
