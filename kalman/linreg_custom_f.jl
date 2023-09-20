3dd43cd1229cb60c8ee7c11598fc1a5c98cbcadd
lik <lik.24@yandex.ru>
2023-09-20 14:04:13 +0500

    fix

--- kalman/linreg_custom_f.jl
+++ kalman/linreg_custom_f.jl
@@ -1,26 +1,34 @@
 function two_step(qw, Pf)
-    yy1, aa = mnk_step1(qw, Pf)
+    yy1, aa, Δaa = mnk_step1(qw, Pf)
     println("1st step MASE: ", round(mean(abs2,Pf.-yy1),digits=3)," MAPE: ", round(mape(Pf,yy1), digits=3))
     yy2 = mnk_step2(qw, yy1, Pf, aa)
     println("2st step MASE: ", round(mean(abs2,Pf.-yy2),digits=3)," MAPE: ", round(mape(Pf,yy2), digits=3))
-    return yy2, aa
+    return yy2, aa, Δaa
 end
 
-function mnk_step1(xx, yy)
+function mnk_step1(xx, yy; critical_value = 0.95)
     nn1 = size(xx,1)
-    nn2 = size(yy,1)
+    nn2 = size(xx,2)
+    nny = size(yy,1)
     xx = vcat(xx,ones(1,size(xx,2)))
     AA = xx*xx'
     println("ранг1: ",rank(AA))
     #ev = eigvals(AA)
     #println(ev," ",extrema(ev))
-    aa = zeros(nn1+1,nn2)
-    for i=1:nn2
+    aa = zeros(nn1+1,nny)
+    for i=1:nny
         BB = xx*yy[i,:]
         aa[:,i] = AA\BB
     end
     yyr = aa'*xx
-    return yyr, aa
+
+    sig = sum(abs2,yyr[:] .- yy[:])/(nn2 - (nn1+1))
+    CovM3x = inv(AA)*sig
+    #println.(eachrow(CovM3x))
+    tp = quantile(TDist(length(xx) - (nn1+1)),(1+critical_value)/2)
+    Δaa = sqrt.(diag(CovM3x))*tp
+
+    return yyr, aa, Δaa
 end
 
 function mnk_step2(xx, yy1, yy, aa)
