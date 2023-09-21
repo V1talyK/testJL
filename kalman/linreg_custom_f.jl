7e837a57213d6e821530d8eb06e9de6849f8da8f
lik <lik.24@yandex.ru>
2023-09-21 13:06:27 +0500

    расчёт ошибки при лин.рег. после тестирования

--- kalman/linreg_custom_f.jl
+++ kalman/linreg_custom_f.jl
@@ -1,9 +1,9 @@
 function two_step(qw, Pf)
-    yy1, aa, Δaa = mnk_step1(qw, Pf)
+    yy1, aa, Δaa, wrn_flag, rnk = mnk_step1(qw, Pf)
     println("1st step MASE: ", round(mean(abs2,Pf.-yy1),digits=3)," MAPE: ", round(mape(Pf,yy1), digits=3))
     yy2 = mnk_step2(qw, yy1, Pf, aa)
     println("2st step MASE: ", round(mean(abs2,Pf.-yy2),digits=3)," MAPE: ", round(mape(Pf,yy2), digits=3))
-    return yy2, aa, Δaa
+    return yy2, aa, Δaa, wrn_flag, rnk
 end
 
 function mnk_step1(xx, yy; critical_value = 0.95)
@@ -12,7 +12,9 @@ function mnk_step1(xx, yy; critical_value = 0.95)
     nny = size(yy,1)
     xx = vcat(xx,ones(1,size(xx,2)))
     AA = xx*xx'
-    println("ранг1: ",rank(AA))
+    rnk = rank(AA)
+    println("ранг1: ",rnk)
+    wrn_flag = rnk!=nn1+1
     #ev = eigvals(AA)
     #println(ev," ",extrema(ev))
     aa = zeros(nn1+1,nny)
@@ -23,12 +25,20 @@ function mnk_step1(xx, yy; critical_value = 0.95)
     yyr = aa'*xx
 
     sig = sum(abs2,yyr[:] .- yy[:])/(nn2 - (nn1+1))
-    CovM3x = inv(AA)*sig
-    #println.(eachrow(CovM3x))
-    tp = quantile(TDist(length(xx) - (nn1+1)),(1+critical_value)/2)
-    Δaa = sqrt.(diag(CovM3x))*tp
+    #println(inv(AA))
+    #println(sig)
+    #println(cov(xx'))
+    if !wrn_flag
+        CovM3x = inv(AA)*sig
+        #println.(eachrow(CovM3x))
+        tp = quantile(TDist(nn2 - (nn1+1)),(1+critical_value)/2)
+        #println(diag(CovM3x))
+        Δaa = sqrt.(diag(CovM3x))*tp
+    else
+        Δaa = NaN*zeros(nn1+1)
+    end
 
-    return yyr, aa, Δaa
+    return yyr, aa, Δaa, wrn_flag, rnk
 end
 
 function mnk_step2(xx, yy1, yy, aa)
@@ -68,4 +78,14 @@ function mnk_step3(xx, yy2, Pf, aa)
     #     yyr[i,:] = bb[:,i]'*PT + aa[:,i]'*xx
     # end
     return yyr
+end
+
+function calc_rank_of_data(xx, fact_i)
+    n = size(xx,1)
+    MR = zeros(n+1,n+1)
+    MR[1:n,1:n].= view(xx,:,fact_i)*view(xx,:,fact_i)'
+    MR[n+1,1:n].=sum(view(xx,:,fact_i),dims=2)
+    MR[1:n,n+1].=MR[n+1,1:n]
+    MR[n+1,n+1] = 1
+    return rank(MR)
 end
